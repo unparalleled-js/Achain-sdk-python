2709643a239550645b3e6d923e6ac0940c1ea921
yangsai <yangsai@aclub.io>
2019-11-13 15:46:37 +0800

    support python2 and python3+

--- achainpy/signer.py
+++ achainpy/signer.py
@@ -1,22 +1,46 @@
 import sys
-from abc import ABC, abstractmethod
+if sys.version > '3':
+    from abc import ABC,abstractmethod
+else:
+    from abc import ABCMeta, abstractmethod
 
-class Signer(ABC):
-    def __init__(self, private_str=''):
-        super().__init__()
+if sys.version > '3':
+    class Signer(ABC):
+        def __init__(self, private_str=''):
+            super().__init__()
     
-    @abstractmethod
-    def to_public(self):
-        pass
+        @abstractmethod
+        def to_public(self):
+            pass
 
-    @abstractmethod
-    def to_wif(self):
-        pass
+        @abstractmethod
+        def to_wif(self):
+            pass
 
-    @abstractmethod
-    def sign(self, digest):
-        pass
+        @abstractmethod
+        def sign(self, digest):
+            pass
     
-    @abstractmethod
-    def verify(self, encoded_sig, digest):
-        pass
+        @abstractmethod
+        def verify(self, encoded_sig, digest):
+            pass
+else:
+    class Signer(ABCMeta):
+        def __init__(self, private_str=''):
+            super().__init__()
+    
+        @abstractmethod
+        def to_public(self):
+            pass
+
+        @abstractmethod
+        def to_wif(self):
+            pass
+
+        @abstractmethod
+        def sign(self, digest):
+            pass
+    
+        @abstractmethod
+        def verify(self, encoded_sig, digest):
+            pass
