985cf5efeef3d2824455fde7fcc45c56a78d1ba4
yangsai <yangsai@aclub.io>
2019-11-21 14:38:36 +0800

    add some operations

---
+++ some_operations/README.md
@@ -0,0 +1,13 @@
+# achain 2.0 
+
+these files are some frequently-used operations
+
+## Transfer assets
+
+```
+# use transfer.py
+python transfer.py --url <specify-your-httpserver>  transfer <sender> <sender-pri-key> <receiver> <quantity> -m <mome> -c <code>
+# python transfer.py --url http://127.0.0.1:8888 transfer act 5JAaSV9atydvYzEdBErjVCVAtx2SYdo3PykoHwrtYA57Xk3QKgQ test '100.0000 ACT' -m "aaaa" -c act.token
+
+```
+## to do
---
+++ some_operations/transfer.py
@@ -0,0 +1,51 @@
+import achainpy.clachain
+import achainpy.keys
+from achainpy.types import Abi, Action
+from achainpy.utils import parse_key_file
+import os
+import pytz
+import json
+import datetime as dt
+import argparse
+
+
+def console_print(data):
+    print(json.dumps(data, indent=4))
+
+parser = argparse.ArgumentParser(description='Command Line Interface to Achain via python')
+parser.add_argument('--api-version','-v', type=str, default='v1', action='store', dest='api_version')
+parser.add_argument('--url', '-u', type=str, action='store', default='https://127.0.0.1:8888', dest='url')
+parser.add_argument('--time-out', type=int, action='store', default=30, dest='timeout')
+subparsers = parser.add_subparsers(dest='subparser')
+### transfer
+trsnafer_parser = subparsers.add_parser('transfer')
+trsnafer_parser.add_argument('sender', type=str, action='store', help='The account to transfer token')
+trsnafer_parser.add_argument('key', type=str, action='store', help='The privite key of sender')
+trsnafer_parser.add_argument('receiver', type=str, action='store', help='The account to receive the token')
+trsnafer_parser.add_argument('quantity', type=str, action='store', help='The amount of the token to transfer, i.g "1.0000 ACT"')
+trsnafer_parser.add_argument('--memo','-m', type=str, action='store', default='',help='The message of transfer token', dest='memo')
+trsnafer_parser.add_argument('--code','-c', type=str, action='store', default='act.token',help='The code of this token belongs to',dest='code')
+# process args
+args = parser.parse_args()
+#
+
+ca = achainpy.clachain.Clachain(url=args.url)
+# run commands based on subparser
+if args.subparser == 'transfer' :
+    arguments = {"from": args.sender, "to": args.receiver, "quantity":  args.quantity,"memo": args.memo}
+    payload = {"account": args.code, "name": "transfer","authorization": [{"actor": args.sender,"permission": "owner",}]}
+    
+    # print(arguments)
+    # print(payload)
+    #Converting payload to binary
+    data = ca.abi_json_to_bin(payload['account'], payload['name'], arguments)
+    #Inserting payload binary form as "data" field in original payload
+    payload['data'] = data['binargs']
+    #final transaction formed
+    trx = {"actions": [payload]}
+    #get expiration
+    trx['expiration'] = str((dt.datetime.utcnow() + dt.timedelta(seconds=60)).replace(tzinfo=pytz.UTC))
+    #get ActKey
+    achainkey = achainpy.keys.ActKey(args.key)
+    resp = ca.push_transaction(trx, achainkey, broadcast=True)
+    console_print(resp)
